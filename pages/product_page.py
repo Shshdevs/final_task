e4d11ca23056148c7fc6841c118fbe68a9441322
HotelkaWants <shurochka.dev@gmail.com>
2026-02-18 20:37:22 +0300

    adding product page

---
+++ pages/product_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+        self.solve_quiz_and_get_code()
+    
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
